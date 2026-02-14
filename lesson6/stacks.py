cbe15fead489ba2b16ca27bede18a56f6690ffaf
MeowyCat1 <samhtfussey@gmail.com>
2026-02-14 16:37:30 +0000

    Add files via upload

---
+++ lesson6/stacks.py
@@ -0,0 +1,42 @@
+class Stack():
+    def __init__(self, n):
+        self.n = n
+        self.list = []
+    def push(self, item):
+        if len(self.list) < self.n:
+            self.list.append(item)
+        else:
+            print("Stack is full")
+    
+    def pop(self):
+        if len(self.list) > 0:
+            self.list.pop()
+        else:
+            print("No items in stack, cannot remove.")
+    
+    def peek(self):
+        if self.n > 0:
+            return self.list[-1]
+        else:
+            print("No items in stack, cannot peek.")
+    
+    def display(self):
+        return self.list
+
+
+mystack = Stack(5)
+
+mystack.push("hello")
+mystack.push("goodbye")
+print(mystack.peek())
+
+mystack.pop()
+print(mystack.peek())
+
+mystack.push("fjnfren")
+mystack.push("fjnfren")
+mystack.push("fjnfren")
+mystack.push("fjnfren")
+mystack.push("fjnfren")
+
+print(mystack.display())
